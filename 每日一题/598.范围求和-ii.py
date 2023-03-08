14b800858f0fa3c79b20e1fa5d77184bf6af1b5d
Leaderzhangyi <zhangyi.upper@qq.com>
2023-03-08 10:44:31 +0800

    馃槑Automatic daily update:2023-03-08,10:44:31

---
+++ 每日一题/598.范围求和-ii.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=598 lang=python3
+#
+# [598] 范围求和 II
+#
+
+from typing import List
+# @lc code=start
+class Solution:
+    def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
+        Mininterval = [40001,40001]
+        if len(ops) == 0:
+            return m * n 
+        for item in ops:
+            if Mininterval[0] >= item[0]:
+                Mininterval[0] = item[0]
+            if Mininterval[1] >= item[1]:
+                Mininterval[1] = item[1]
+       # print(Mininterval)
+        return Mininterval[0] * Mininterval[1]
+
+# @lc code=end
+
+print(Solution().maxCount( m = 3, n = 3, ops = [[2,2],[3,3],[3,3],[3,3],[2,2],[3,3],[3,3],[3,3],[2,2],[3,3],[3,3],[3,3]]))
