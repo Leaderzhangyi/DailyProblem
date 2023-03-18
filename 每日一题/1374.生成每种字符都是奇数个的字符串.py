f17281717d31de51b3317decaad5b547217f99c9
Leaderzhangyi <zhangyi.upper@qq.com>
2023-03-18 18:21:21 +0800

    Automatic daily update:2023-03-18,18:21:21

---
+++ 每日一题/1374.生成每种字符都是奇数个的字符串.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=1374 lang=python3
+#
+# [1374] 生成每种字符都是奇数个的字符串
+#
+
+# @lc code=start
+class Solution:
+    def generateTheString(self, n: int) -> str:
+        if n & 1: #奇数
+            return 'a' * n 
+        else:
+            return 'a' * (n-1) + 'z'
+# @lc code=end
+
