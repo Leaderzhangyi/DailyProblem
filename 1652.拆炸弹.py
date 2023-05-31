3b3ffecd1d540ee5da8924c94066b00fff45f0b7
Leaderzhangyi <zhangyi.upper@qq.com>
2023-05-31 20:47:15 +0800

    Automatic daily update:2023-05-31,20:47:15

---
+++ 1652.拆炸弹.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode.cn id=1652 lang=python3
+#
+# [1652] 拆炸弹
+#
+
+# @lc code=start
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+# @lc code=end
+
---
+++ 每日一题/1678.设计-goal-解析器.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode.cn id=1678 lang=python3
+#
+# [1678] 设计 Goal 解析器
+#
+
+# @lc code=start
+class Solution:
+    def interpret(self, command: str) -> str:
+        return command.replace("()","o").replace("(al)","al")
+    
+
+# @lc code=end
+
