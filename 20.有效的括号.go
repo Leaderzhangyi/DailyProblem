c8a831d25ea0b483f5f4cc90b9f893a39822d7a4
Leaderzhangyi <zhangyi.upper@qq.com>
2024-09-21 15:21:10 +0800

    Automatic daily update:2024-09-21,15:21:10

--- 20.有效的括号.go
+++ 20.有效的括号.go
@@ -10,17 +10,7 @@ func isValid(s string) bool {
 	for _, c := range s {
 		if c == '(' || c == '[' || c == '{' {
 			stack = append(stack, byte(c))
-		} else if len(stack) == 0 {
-			return false
-		} else if c == ')' && stack[len(stack)-1] == '(' ||
-			c == ']' && stack[len(stack)-1] == '[' ||
-			c == '}' && stack[len(stack)-1] == '{' {
-			stack = stack[:len(stack)-1]
-		} else {
-			return false
-		}
-	}
-	return len(stack) == 0
+		} 
 
 }
 
